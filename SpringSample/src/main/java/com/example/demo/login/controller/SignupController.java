4dc3fe1c9bb92298c4637a02ed1a630a2c7fddb2
Masashi <t15519ms@gmail.com>
2020-09-13 22:24:18 +0900

    add: validation

--- SpringSample/src/main/java/com/example/demo/login/controller/SignupController.java
+++ SpringSample/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
@@ -35,7 +37,7 @@ public class SignupController {
 	}
 
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form,
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form,
 			BindingResult bindingResult, Model model) {
 		//データバインド失敗の場合
 		//入力チェックに引っかかった場合
