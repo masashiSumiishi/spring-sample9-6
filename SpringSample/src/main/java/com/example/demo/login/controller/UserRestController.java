11deeb416cd732e3dd6c250cda38e08b95f79b83
Masashi <t15519ms@gmail.com>
2020-09-24 22:46:20 +0900

    add: Mybatis

--- SpringSample/src/main/java/com/example/demo/login/controller/UserRestController.java
+++ SpringSample/src/main/java/com/example/demo/login/controller/UserRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.login.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.example.demo.login.domain.service.RestService;
 public class UserRestController {
 
 	@Autowired
+	@Qualifier("RestServiceMybatisImpl")
 	RestService service;
 
 	//全件取得
---
+++ SpringSample/src/main/java/com/example/demo/login/domain/repository/mybatis/UserMapper2.java
@@ -0,0 +1,14 @@
+package com.example.demo.login.domain.repository.mybatis;
+
+import java.util.List;
+
+import com.example.demo.login.domain.model.User;
+
+public interface UserMapper2 {
+
+	public boolean insert(User user);
+	public User selectOne(String userId);
+	public List<User> selectMany();
+	public boolean updateOne(User user);
+	public boolean deleteOne(String userId);
+}
